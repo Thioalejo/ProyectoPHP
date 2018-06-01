d69a42c349eac92d6a347bbf9a38b7e530d04572
miguelalegria <miguel.alegria@areamovil.com.co>
2018-06-01 00:53:33 -0500

    Se implemento ofertas en oulet en la vista oulet, se probo con registros insertados directamente en la base de datos falta crear vista para registras los productos

--- usuarios/oulet.php
+++ usuarios/oulet.php
@@ -5,6 +5,7 @@
 	<title></title>
 	
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<LINK REL=StyleSheet HREF="bootstrap\css\cartas.css" TYPE="text/css" MEDIA=screen>
 </head>
 <body>
 
@@ -58,9 +59,57 @@
 		 </p> 
 		</div>
 
-		<div class="container">
-			
-		</div>
+		<div  class="container page-header text-center">
+
+		<p>
+				 	<h2>Productos y ofertas</h2>
+				<?php 
+							 
+								//incluimos la conexion a la BD
+								//include_once('C:\xampp\htdocs\ProyectoPHP\BD\Conexion.php');
+
+									$SQL = "SELECT * FROM carnesfrias.producto";
+									//$SQLCont= "SELECT count(*) FROM carnesfrias.cliente"
+
+									
+									$Result = $db->query($SQL);
+									//$ResultCont = $db->query($SQLCont);
+
+									while($row = $Result->fetch_array(MYSQLI_ASSOC))
+									{
+										//$rowCont = $ResultCont->fetch_array(MYSQLI_ASSOC);
+										
+										$nombre_producto =	$row['NombreProducto'];
+										$urlImagen = $row['urlImagen'];
+										$descripcion =	$row['Presentacion'];
+										$precio = $row['Precio'];
+
+
+
+		?>
+		 								<div class="contenedor" >
+												<div class="contenido" style="width:92%" >
+													 <?php echo "$nombre_producto"?>
+
+													  
+													  <img src=<?php echo "$urlImagen" ?> alt="Norway" style="width:100%">
+													  
+													    <p><?php echo "$descripcion" ?></p>
+													  
+													    <p>Precio: <?php echo '$ '. "$precio" ?></p>
+												</div>
+											</div>
+									<?php 	
+									}     
+							     ?>
+		</p>
+
+
+
+
+
+
+</div>
 
 
  </body>
